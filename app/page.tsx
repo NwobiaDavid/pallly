f35259cde9d6d361182e388404a36b1006e11d99
NwobiaDavid <dnwobia@gmail.com>
2024-04-08 01:50:45 +0100

    almost done with the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
   }
 
   return (
-    <main className="flex h-screen flex-col items-center justify-between lg:p-4 p-2 bg-purple-300 to-blue-500 bg-gradient-to-tr ">
+    <main className="flex h-screen flex-col items-center justify-between lg:p-4 p-2  bg-black ">
 
       {/* hey there
       {count}
@@ -94,37 +94,39 @@ export default function Home() {
       </div> */}
 
       <div className="h-[6%] ">
-        <h1 className=" lg:text-3xl capitalize font-bold " >pally.</h1>
+        <h1 className=" lg:text-3xl capitalize font-bold text-white " >pally.</h1>
       </div>
-      <div className="border bg-white relative p-2 lg:px-4 rounded-xl lg:justify-between justify-center items-center flex flex-col lg:flex-row w-full h-[94%] ">
+      <div className="border bg-white relative p-2 lg:px-5 lg:py-3 rounded-xl lg:justify-between justify-center items-center flex flex-col lg:flex-row w-full h-[94%] ">
 
         <div className="flex flex-col lg:justify-between lg:w-[70%] h-full z-20 ">
           <div>
-            <div className=" p-1 " >
-              <h2 className="text-3xl lg:text-9xl font-bold " >Tired of Starting Goals You Never Finish?</h2>
-              <h3 className="text-xl lg:text-4xl opacity-80 " >We don't promise magic. But we do offer something powerful: accountability that actually works.</h3>
+            <div className=" p-1  " >
+              <h2 className="text-3xl lg:text-7xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 text-transparent bg-clip-text " >Tired of Starting Goals You Never Finish?</h2>
+              <h3 className="text-xl w-[90%] lg:text-3xl opacity-60  " >We don't promise <span className="bg-gradient-to-tr from-purple-700 bg-clip-text bg-transparent to-pink-500 ">magic</span>. But we do offer something powerful:<span className="font-semibold"> accountability that actually works</span>.</h3>
             </div>
             <div className=" p-1 lg:mt-10 mt-5  lg:text-lg mb-16 ">
-              <h4>We understand the frustration of starting a new goal with excitement, only to see motivation fizzle out a few weeks later. Pally solves this by connecting you with an accountability partner who shares your journey, keeping you focused and motivated for the long haul.</h4>
-              <p> <IoCheckmarkDone /> PALLLY connects you with a partner based on your goals, interests, and preferred communication style</p>
-              <p className="my-1" > <IoCheckmarkDone /> Schedule daily or weekly check-ins, set friendly challenges, and send encouraging messages to keep each other motivated.</p>
-              <p className="my-1" > <IoCheckmarkDone /> Monitor your progress toward your goals, stay focused, and celebrate milestones along the way.</p>
+              <h4 className=" tracking-wider mb-16 w-[70%] " >We understand the frustration of starting a new goal with excitement, only to see motivation fizzle out a few weeks later. Pally solves this by connecting you with an accountability partner who shares your journey, keeping you focused and motivated for the long haul.</h4>
+              <div className="w-[80%]  ">
+                <p className="flex items-center"> <Image className="mr-2" alt="check" src={"/props/check.svg"} width={20} height={20} />Pallly connects you with a partner based on your goals, interests, and preferred communication style</p>
+                <p className="my-3 flex  items-center " > <Image className="mr-2" alt="check" src={"/props/check.svg"} width={20} height={20} /> Schedule daily or weekly check-ins, set friendly challenges, and send encouraging messages to keep each other motivated.</p>
+                <p className=" flex items-center" ><Image className="mr-2" alt="check" src={"/props/check.svg"} width={20} height={20} /> Monitor your progress toward your goals, stay focused, and celebrate milestones along the way.</p>
+              </div>
             </div>
           </div>
 
-          <div className=" lg:mb-[10%] mt-12 flex flex-col lg:ml-[10%] ">
-            <p>stop dreaming, start achieving. be notified when we go live. sign up now! </p>
-            <div className="relative border w-full lg:w-[50%] bg-white overflow-hidden flex items-center rounded-full  ">
-              <input className="p-2 lg:p-4 outline-none w-[80%] lg:w-[86%] " type="email" placeholder="email" name="" id="" />
-              <button className="absolute px-3 lg:px-5 lg:py-3 py-1 my-1 rounded-full right-[10px] bg-black text-white " >send</button>
-            </div>
+          <div className=" lg:mb-[10%] mt-12 flex flex-col  ">
+            <p className="ml-2 text-sm font-semibold " >Stop dreaming, start achieving. Be notified when we go live. Sign up now! </p>
+            {/* <div > */}
+              <form className="relative border w-full lg:w-[60%] bg-white overflow-hidden flex items-center rounded-2xl  " onClick={postData} action="POST">
+              <input className="p-2 lg:p-4 outline-none w-[80%] lg:w-[86%] " type="email" placeholder="example@example.com" name="" id="" />
+              <button className="absolute px-3 lg:px-6 lg:py-3 py-1 my-1 rounded-2xl right-[10px] bg-gradient-to-tr from-blue-600 to-purple-700 hover:bg-black duration-200 text-white flex items-center   " >send <Image className=" ml-1" alt="check" src={"/props/send.png"} width={20} height={20} /> </button>
+              </form>
+            {/* </div> */}
           </div>
         </div>
 
         <div className=" h-full lg:top-0 lg:right-[10%] top-[20%] absolute flex justify-center items-center  " >
-          <div className="h-[200px] lg:h-[700px]  lg:w-[700px] w-[200px] bg-red-600 rotate-12 " >
-
-          </div>
+          <Image className="  " alt="main image" src={"/props/phone.png"} width={400} height={400} />
         </div>
 
       </div>
