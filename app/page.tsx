6b7c9703f6a39fbb6570d8bb25e1c878822341ff
NwobiaDavid <dnwobia@gmail.com>
2024-04-11 00:21:20 +0100

    made some responsive changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,8 @@ export default function Home() {
   const [showPopUp, setShowPopUp] = useState(false); // State to control pop-up visibility
 
   useEffect(() => {
+      // const countt = getCount()
+  // console.log("THE COUNT IS => " + countt.then())
     let responseData;
     const fetchCount = async () => {
       try {
@@ -20,11 +22,15 @@ export default function Home() {
           headers: {
             "Content-Type": "application/json",
           },
+          cache: 'no-store'
         });
+
         responseData = await res.json();
+        
         if (res.status !== 200) {
           throw new Error(responseData.message);
         }
+
         setCount(responseData.count);
       } catch (error) {
         console.log("Error: ", error);
@@ -95,6 +101,8 @@ export default function Home() {
     setIsValidEmail(validateEmail(inputEmail));
   }
 
+
+
   return (
     <>
       <main className="flex h-screen flex-col items-center justify-between lg:p-4 p-2 overflow-hidden relative  bg-black ">
@@ -110,47 +118,47 @@ export default function Home() {
         <div className="h-[6%]  ">
           <h1 className=" text-3xl capitalize font-bold text-white " >pallly..</h1>
         </div>
-        <div className="border bg-white relative p-2 lg:px-8 lg:py-3 rounded-xl lg:justify-between justify-center items-center flex flex-col lg:flex-row w-full h-[94%] ">
+        <div className="border overflow-hidden bg-white relative p-2 lg:px-8 lg:py-3 rounded-xl lg:justify-between justify-center items-center flex flex-col lg:flex-row w-full h-[94%] ">
 
           <div className="flex flex-col xl:justify-between xl:w-[70%] h-full z-20 ">
             <div>
               <div className=" p-1 text-center lg:text-left " >
-                <h2 className="text-3xl xl:text-7xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 text-transparent bg-clip-text " >Tired of Starting Goals You Never Finish?</h2>
+                <h2 className=" 3xl:text-9xl text-3xl lg:text-5xl xl:text-5xl 2xl:text-7xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 text-transparent bg-clip-text " >Tired of Starting Goals You Never Finish?</h2>
                 <h3 className="text-lg w-[90%] xl:text-3xl opacity-60  " >We don't promise <span className="bg-gradient-to-tr from-purple-700 font-bold to-pink-600 text-transparent bg-clip-text ">magic</span>. But we do offer something powerful:<span className="font-bold"> accountability that actually works</span>.</h3>
               </div>
-              <div className=" p-1 lg:mt-10 mt-5  lg:text-lg lg:mb-16 ">
-                <h4 className=" lg:tracking-wider hidden lg:flex lg:mb-16 lg:w-[70%] " >We understand the frustration of starting a new goal with excitement, only to see motivation fizzle out a few weeks later. Pally solves this by connecting you with an accountability partner who shares your journey, keeping you focused and motivated for the long haul.</h4>
+              <div className=" p-1 3xl:mt-24 lg:mt-10 mt-5  lg:text-lg lg:mb-16 ">
+                <h4 className=" lg:tracking-wider hidden lg:flex 3xl:mb-32 lg:mb-16 lg:w-[70%] xl:w-[80%] 3xl:text-4xl " >We understand the frustration of starting a new goal with excitement, only to see motivation fizzle out a few weeks later. Pally solves this by connecting you with an accountability partner who shares your journey, keeping you focused and motivated for the long haul.</h4>
                 <div className="lg:w-[80%]  ">
-                  <p className="flex items-center"> <Image className="mr-2" alt="check" src={"/props/check.svg"} width={20} height={20} />Pallly connects you with a partner based on your goals, interests, and preferred communication style</p>
-                  <p className="my-3 flex  items-center " > <Image className="mr-2" alt="check" src={"/props/check.svg"} width={20} height={20} /> Schedule daily or weekly check-ins, set friendly challenges, and send encouraging messages to keep each other motivated.</p>
-                  <p className=" flex items-center" ><Image className="mr-2" alt="check" src={"/props/check.svg"} width={20} height={20} /> Monitor your progress toward your goals, stay focused, and celebrate milestones along the way.</p>
+                  <p className="flex items-center 3xl:text-4xl "> <Image className="mr-2 3xl:w-[60px] " alt="check" src={"/props/check.svg"} width={20} height={20} />Pallly connects you with a partner based on your goals, interests, and preferred communication style</p>
+                  <p className="my-3 flex  items-center 3xl:text-4xl  " > <Image className="mr-2 3xl:w-[60px] " alt="check" src={"/props/check.svg"} width={20} height={20} /> Schedule daily or weekly check-ins, set friendly challenges, and send encouraging messages to keep each other motivated.</p>
+                  <p className=" flex items-center 3xl:text-4xl  " ><Image className="mr-2 3xl:w-[60px] " alt="check" src={"/props/check.svg"} width={20} height={20} /> Monitor your progress toward your goals, stay focused, and celebrate milestones along the way.</p>
                 </div>
               </div>
             </div>
 
-            <div className=" lg:mb-[10%] mt-12 flex flex-col  ">
-              <p className="ml-2 text-sm font-semibold " >Stop dreaming, start achieving. Be notified when we go live. Sign up now! </p>
+            <div className=" lg:mb-[10%]   mt-12 flex flex-col  ">
+              <p className="ml-2 text-sm 3xl:text-2xl font-semibold " >Stop dreaming, start achieving. Be notified when we go live. Sign up now! </p>
               {/* <div > */}
-              <form className="relative border w-full lg:w-[60%] bg-white overflow-hidden flex items-center rounded-2xl  " onSubmit={(e) => postData(e)} >
-                <input value={email} onChange={handleEmailChange} className="p-2 lg:p-4 outline-none w-[80%] lg:w-[86%] " type="email" placeholder="example@example.com" name="" id="" />
-                <button type="submit" className="absolute px-3 lg:px-6 lg:py-3 py-1 my-1 rounded-2xl right-[10px] bg-gradient-to-tr from-blue-600 to-purple-700 hover:from-black hover:to-black duration-300 text-white flex items-center   " >
+              <form className="relative border w-full lg:w-[60%] 3xl:w-[80%] bg-white overflow-hidden flex items-center rounded-2xl  " onSubmit={(e) => postData(e)} >
+                <input value={email} onChange={handleEmailChange} className="p-2 lg:p-4 3xl:p-10 3xl:text-2xl outline-none w-[80%] lg:w-[86%] " type="email" placeholder="example@example.com" name="" id="" />
+                <button type="submit" className="absolute px-3 lg:px-6 lg:py-3 3xl:py-5 3xl:px-10 3xl:text-2xl py-1 my-1 3xl:rounded-3xl rounded-2xl right-[10px] bg-gradient-to-tr from-blue-600 to-purple-700 hover:from-black hover:to-black duration-300 text-white flex items-center   " >
                   {isLoading ? (
                     <span>Loading...</span>
                   ) : (
                     <>
                       <span>Send</span>
-                      <Image className="ml-1" alt="check" src={"/props/send.png"} width={20} height={20} />
+                      <Image className="ml-1 3xl:text-2xl " alt="check" src={"/props/send.png"} width={20} height={20} />
                     </>
                   )}
                 </button>
               </form>
-              <p className="font-semibold ml-2 text-xs lg:text-left text-center " >({count} people signed up)</p>
+              <p className="font-semibold ml-2 text-xs lg:text-left 3xl:text-xl text-center " >({count} people signed up)</p>
               {/* </div> */}
             </div>
           </div>
 
           <div className=" h-full lg:top-0 opacity-50 lg:opacity-100 lg:right-[5%] -bottom-52 absolute flex justify-center items-center  " >
-            <Image className="  " alt="main image" src={"/props/phone.png"} width={700} height={700} />
+            <Image className=" 3xl:w-[1000px] lg:w-[500px] " alt="main image" src={"/props/phone.png"} width={700} height={700} />
           </div>
 
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        'xl': '1360px',
+        '3xl': '2000px',
+      },
     },
   },
   plugins: [],
